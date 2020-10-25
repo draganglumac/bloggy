bd03af7268f8549d747a5b35b25c07e271f9265d
Dragan Glumac <dragan.glumac@gmail.com>
2020-10-25 17:46:09 +0000

    Internationalisation and Swagger 2

--- src/main/kotlin/uk/co/gluedit/bloggy/BloggyApplication.kt
+++ src/main/kotlin/uk/co/gluedit/bloggy/BloggyApplication.kt
@@ -2,10 +2,29 @@ package uk.co.gluedit.bloggy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.support.ResourceBundleMessageSource
+import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver
+import java.util.*
 
 @SpringBootApplication
 class BloggyApplication
 
 fun main(args: Array<String>) {
 	runApplication<BloggyApplication>(*args)
+}
+
+@Bean
+fun localeResolver(): LocaleResolver {
+	val localeResolver = AcceptHeaderLocaleResolver()
+	localeResolver.defaultLocale = Locale.UK
+	return localeResolver
+}
+
+@Bean
+fun bundleMessageSource(): ResourceBundleMessageSource {
+	val messageSource = ResourceBundleMessageSource()
+	messageSource.setBasename("messages")
+	return messageSource
 }
--- src/main/kotlin/uk/co/gluedit/bloggy/controllers/HelloWorldController.kt
+++ src/main/kotlin/uk/co/gluedit/bloggy/controllers/HelloWorldController.kt
@@ -1,25 +1,26 @@
 package uk.co.gluedit.bloggy.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import uk.co.gluedit.bloggy.model.HelloWorldBean
+import java.util.*
 
 @RestController
-class HelloWorldController {
+class HelloWorldController(@Autowired val messageSource: MessageSource) {
 
     @GetMapping("hello-world")
-    fun helloWorld(): String {
-        return "Hello world!"
-    }
+    fun helloWorld() = messageSource.getMessage("hello.world", null, LocaleContextHolder.getLocale())
 
     @GetMapping("hello-world-bean")
-    fun helloWorldBean(): HelloWorldBean {
-        return HelloWorldBean("Hello world!")
-    }
+    fun helloWorldBean() = HelloWorldBean(helloWorld())
 
     @GetMapping("hello/{name}")
-    fun hello(@PathVariable name: String): HelloWorldBean {
-        return HelloWorldBean("Hello ${name}!")
-    }
+    fun hello(@PathVariable name: String) = HelloWorldBean(
+            "${messageSource.getMessage("hello", null, LocaleContextHolder.getLocale())} ${name}!"
+    )
 }
